dd22de3c60df7dceded11d4e2ad126d1f96d19de
Jonny Sjöberg <jonny.sjoberg.229@student.lu.se>
2015-02-23 15:01:51 +0100

    fixed denavit-hartenberg and verified it's doing as it should by rotating each joint manually by 45 degrees and checking with my derivations how the tool should be placed and oriented

--- source/denavit-hartenberg.py
+++ source/denavit-hartenberg.py
@@ -3,6 +3,7 @@ from __future__ import division
 import numpy as n
 from numpy import cos, sin, pi
 from numpy import array as mat
+#--------------------------#
 import operator
 #--------------------------#
 def prod(factors):
@@ -10,79 +11,59 @@ def prod(factors):
 #--------------------------#
 def matmul(matrix_factors):
 	return reduce(n.dot, matrix_factors, 1)
-
-def round(v, prec=1e-4):
-    factor = 1/prec
-    if v > 0:
-        return n.int32(v * factor + 0.5)/factor
-    elif v < 0:
-        return n.int32(v * factor - 0.5)/factor
-    return 0
-#--------------------------#
-def list_round(L):
-    sh = L.shape
-    L = L.reshape(prod(sh))
-    res = mat(map(round, L)).reshape(sh)
-    return res
-#--------------------------#
-def rotZj_theta_i(theta_i):
-    c = lambda: cos(theta_i)
-    s = lambda: sin(theta_i)
-    return mat([[c(), -s(), 0, 0],
-                [s(),  c(), 0 , 0],
-                [0,      0, 1,  0],
-                [0,      0, 0,  1]])    
-
-def rotXi_alpha_i(alpha_i):
-    c = lambda: cos(alpha_i)
-    s = lambda: sin(alpha_i)
-    return mat([[1, 0, 0, 0],
-                [0, c(), -s(), 0],
-                [0, s(),  c(), 0],
-                [0,   0,    0, 1]])
-
-def transZj_di(d_i):
-    return mat([[1, 0, 0,    0],
-                [0, 1, 0,    0],
-                [0, 0, 1,  d_i],
-                [0, 0, 0,    1]])    
-
-def transXi_ai(a_i):
-    return mat([[1, 0, 0,  a_i],
-                [0, 1, 0,    0],
-                [0, 0, 1,    0],
-                [0, 0, 0,    1]])
-
-def DHji(ai, alphai, di, thetai):
-    res = rotZj_theta_i(thetai)
-    res = res.dot( transZj_di(di) )
-    res = res.dot( transXi_ai(ai) )
-    res = res.dot( rotXi_alpha_i(alphai) )
-    return res
-
+#=====================================================#
+import sys
+sys.path.append("../int/misc-tools/")
+import parsingtools as  parse
+#----------------------------------------------------------------------------------------------------------#
+def mat_trans_x( tx ):
+    return mat([[1,        0,      0,     tx],
+                     [0,        1,      0,      0],
+                     [0,        0,      1,      0],
+                     [0,        0,      0,      1]])
+#----------------------------------------------------------------------------------------------------------#
+def mat_trans_z( tz ):
+    return mat([[1,     0,     0,      0],
+                     [0,     1,     0,      0],
+                     [0,     0,     1,      tz],
+                     [0,     0,     0,      1]])
+def mat_rot_x( ang ):
+    #convert to radians
+    c = cos(ang * pi / 180)
+    s = sin(ang * pi / 180)
+    return mat([[1,     0,      0,      0],
+                     [0,     c,     -s,     0],
+                     [0,     s,     c,      0],
+                     [0,     0,     0,      1]])
+#----------------------------------------------------------------------------------------------------------#
+def mat_rot_z( ang ):
+    #convert to radians
+    c = cos(ang * pi / 180)
+    s = sin(ang * pi / 180)
+    return mat([[c,     -s,    0,     0],
+                     [s,     c,     0,     0],
+                     [0,     0,     1,     0],
+                     [0,     0,     0,     1]])
+#----------------------------------------------------------------------------------------------------------#
+def Aji(A, alpha, D, theta):
+    Rz_J = mat_rot_z(theta)
+    Tz_J = mat_trans_z(D)
+    Tx_I = mat_trans_x(A)
+    Rx_I = mat_rot_x(alpha)
+    return n.round((Rz_J.dot(Tz_J).dot(Tx_I).dot(Rx_I))*1000)/1000.0
+#----------------------------------------------------------------------------------------------------------#
 def DH(a,b,c,d,e,f):
-    A01 = DHji(0,   pi/2,   0.290,    pi+a)
-    A12 = DHji(0.340, 0,      0,      pi/2+b)
-    A23 = DHji(0,   pi/2,   0,      pi+c)
-    A34 = DHji(0,   pi/2,   0.302,    pi+d)
-    A45 = DHji(0,   pi/2,   0,      pi+e)
-    A56 = DHji(0,   0,      0.72,     f)
-    matrices = (A01, A12, A23, A34, A45, A56)
-    return matmul(matrices)
-
-def pr(s):
-    print list_round(s)
-
+    a1 = Aji(0,90,290,180+a) #ok
+    a2 = Aji(340,0,0,90+b) #ok
+    a3 = Aji(0, 90, 0, 180+c) #ok
+    a4 = Aji(0, 90, 302, 180+d) #ok
+    a5 = Aji(0, 90, 0, 180+e) #ok
+    a6 = Aji(0, 0, 72, 0+f) #ok
+    a = a1.dot(a2).dot(a3).dot(a4).dot(a5).dot(a6) #ok
+    return a
+#----------------------------------------------------------------------------------------------------------#
+def vec_to_mat44(v):
+    return v.reshape((4,4))
+#----------------------------------------------------------------------------------------------------------#
 if __name__ == '__main__':
-    T44 = mat([[0.364179993608069, -0.88395872629214, -0.293240349317084, 0],
-                [0.594536079422304, 0.463016221588474, -0.657375713584773, 0],
-                [0.716868037033862, 0.0650611155599963, 0.694166600119385, 0],
-                [0.0836863220332209, -0.0181462742616406, 0.648626596909796,1]]).T
-    j0 = -35.4455740562938
-    j1 = -56.7487189266047
-    j2 =  38.5964427732583
-    j3 = -43.7627466350371
-    j4 = -42.6628949333683
-    j5 = -0.0244652973799493
-    T = DH(j0,j1,j2,j3,j4,j5)
-    
+    data = parse.parse_file("C:\\robot-studio-output.txt")
