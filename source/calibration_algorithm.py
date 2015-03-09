3cf978312378626b42b22d1dafb213261873dd04
Jonny Sjöberg <jonny.sjoberg.229@student.lu.se>
2015-03-09 18:04:13 +0100

    rewriting calibration algorithm to make it more readable, also found a bug which made the code not do LMS-solving

--- source/calibration_algorithm.py
+++ source/calibration_algorithm.py
@@ -1,162 +1,95 @@
 from __future__ import division
+from helperfunctions import *
+import numpy
 #----------------------------------------#
-from scipy import *
-from numpy import *
-from pylab import *
-from numpy.linalg import *
-from numpy.random import rand
-#----------------------------------------#
-def rad(x):
-    return x * pi / 180
-#----------------------------------------#
-def Rz(angle):
-    angle = rad(angle)
-    return array([[ cos(angle), -sin(angle), 0 ],
-                        [ sin(angle),  cos(angle), 0 ],
-                        [  0   ,    0  , 1 ]])
-#----------------------------------------#
-def Rx(angle):
-    angle = rad(angle)
-    return array([[ 1,     0     ,           0 ],
-                  [ 0, cos(angle), -sin(angle) ],
-                  [ 0, sin(angle),  cos(angle)]])
-#----------------------------------------#
-'''
-    creates a rotation mapping from subspace to worldspace
-    using euler angles in degrees
-'''
-def R(rot, tilt, skew):
-    return dot(Rx(rot), dot(Rz(tilt), Rx(skew)))
-#----------------------------------------#
-'''
-    creates a rotation mapping from subspace to worldspace
-    using vector directions
-'''
-def Rv(dirz, diry):
-    dirz = dirz / norm(dirz)
-    diry = diry / norm(diry)
-    diry = grahm_schm(diry, dirz)
-    dirx = cross(diry, dirz)
-    return array([dirx, diry, dirz]).T
-#----------------------------------------#
-'''
-    Orthogonal projection of vector a onto vector b
-'''
-def P(a, b):
-    c = b / norm(b)
-    return a.dot(c) * c
-#----------------------------------------#
-'''
-    Calculates the orthogonal component of A relative to the
-    parallell direction vector B
-'''
-def grahm_schm(a, b):
-    return a - P(a, b)
-#----------------------------------------#
-def define_plane(origin, dirx, diry):
-    origin = array(origin); dirx = array(dirx); diry = array(diry)
-    normal = cross(dirx, diry)
-    normal = normal / norm(normal)
-    diry = grahm_schm(diry, dirx)
-    diry = diry / norm(diry)
-    dirx = dirx / norm(dirx)
-    return [origin, dirx, diry, normal]
-#----------------------------------------#
-def init_plot():
-    from mpl_toolkits.mplot3d import Axes3D
-    import matplotlib.pyplot as plt
-    fig = plt.figure()
-    ax = fig.gca(projection='3d') 
-    return ax, fig
-#----------------------------------------#
-def plot_plane(ax, plane):
-    dx = array([get_plane_point(plane,0,0), get_plane_point(plane,1,0)])
-    dy = array([get_plane_point(plane,0,0), get_plane_point(plane,0,1)])
-    dz = array([get_plane_point(plane,0,0), get_plane_point(plane,0,0) + plane[3]])
-    ax.plot(dx[:,0],dx[:,1],dx[:,2],'b')
-    ax.plot(dy[:,0],dy[:,1],dy[:,2],'g')
-    ax.plot(dz[:,0],dz[:,1],dz[:,2],'r')
+num_points = 100
 #----------------------------------------#
 def get_plane_point(plane,x,y):
-    origin, dx, dy, n = plane
-    pos = origin + x*dx + y*dy
+    """
+        From a subspace plane coordinate system with local coordinates (x,y)
+        return the coordinates in corresponding world-space.
+    """
+    origin, basis_x, basis_y, _ = plane
+    pos = origin + x*basis_x+ y*basis_y
     return pos
-#----------------------------------------#
-def get_relative_point(plane,x,y,Dx,Dy,L):
-    origin, dx, dy, n = plane
-    pos = get_plane_point(plane,x,y)
-    vec = n + Dx*dx + Dy*dy
-    vec = vec / norm(vec)
-    rel = pos + L*vec
-    dir = pos - rel
-    dir = dir / norm(dir)
-    return rel, dir, pos,(dx*x,dy*y)
 #========================================#
 print; print "Init plots..."
 ax,_ = init_plot()
-#========================================#
+#----------------------------------------#
 print; print "Define planes..."
-pl = define_plane([1,1,1],[2,-2,-1],[1,3,0])
-o, dx, dy, n = pl
+transformed_paper = define_plane([1,1,1],[2,-2,-1],[1,3,0])
+(origin_transformed_paper, basis_x_transformed_paper,
+ basis_y_transformed_paper, normal_transformed_paper) = transformed_paper
 
-paper = define_plane([0,0,0],[1,0,0],[0,1,0])
-op, dxp, dyp, np = paper
+untransformed_paper = define_plane([0,0,0],[1,0,0],[0,1,0])
+(origin_paper, basis_x_paper, basis_y_paper, normal_paper) = untransformed_paper
 #----------------------------------------#
 print; print "Placing points in coordinate systems.."
-p = []
-p_paper = []
-num_points = 4
-for i in range(0, num_points):
-    x = rand(); y = rand()
-    p.append(get_plane_point(pl,x,y))
-    p_paper.append(get_plane_point(paper,x,y))
-p = array(p)
-p_paper = array(p_paper)
+transformed_point = []
+untransformed_point = []
+for i in xrange(0, num_points):
+    x, y = (rand(), rand())
+    transformed_point.append( get_plane_point(transformed_paper,x,y) )
+    untransformed_point.append( get_plane_point(untransformed_paper,x,y) )
+
+transformed_point = array(transformed_point)
+untransformed_point = array(untransformed_point)
 ###========================================#
-#picks directions in both plane coordinates
-p_dir = p[0] - p[1], p[1] - p[2]
-p_paper_dir = p_paper[0] - p_paper[1], p_paper[1] - p_paper[2]
-p_dir = array(p_dir)
-p_paper_dir = array(p_paper_dir)
-p_paper_dir = p_paper_dir[:,0:2] #no z-coord
+##  picks directions in both plane coordinates,
+##  depecated code only tested 3 points
+##index = numpy.int32(rand()*(num_points-3))
+##print "index picked: " + str(index)
+##transformed_directions = transformed_point[index] - transformed_point[index+1], transformed_point[index+1] - transformed_point[index+2]
+##untransformed_directions = untransformed_point[index] - untransformed_point[index+1], untransformed_point[index+1] - untransformed_point[index+2]
+###========================================#
+transformed_directions = -diff(transformed_point.T).T
+untransformed_directions = -diff(untransformed_point.T).T
+untransformed_directions = untransformed_directions[:,0:2] #no z-coord
 #----------------------------------------#
 print; print "solve for basis in world coordinates from our sampled directions:"
 if num_points == 3: #exact solution possible, (num_points - 1) * 3 direction eqs, (num_points * 2) unknowns
-    plane_basis = linalg.solve(p_paper_dir, p_dir)
+    print "-> solving exact..."
+    plane_basis = linalg.solve(untransformed_directions, transformed_directions)
 elif num_points > 3: #overdetermined system, ((num_points - 1) - 2) * 3 direction eqs extra and (num_points - 3) * 2 unknowns extra
-    plane_basis = linalg.solve(p_paper_dir.T.dot(p_paper_dir), p_paper_dir.T.dot(p_dir))
+    print "-> solving with LMS..."
+    plane_basis = linalg.solve(untransformed_directions.T.dot(untransformed_directions), untransformed_directions.T.dot(transformed_directions))
 else: #Underdetermines system
-    raise "Underdetermined system - too few points."
-print plane_basis[0], plane_basis[1]
-print dx, dy
+    raise Exception("Underdetermined system - too few points.")
+
+plane_basis_error = numpy.abs(plane_basis[0] - basis_x_transformed_paper, plane_basis[1] - basis_y_transformed_paper)
+print "plane basis vectors error = " + str(plane_basis_error)
 #----------------------------------------#
-print; print "Solve for the paper origin in world coordinates:"
-p_pos = p[0], p[1], p[2]
-p_paper_pos = p_paper[0], p_paper[1], p_paper[2]
+print; print "Solve for the transformed_paper origin in world coordinates:"
+p_pos = transformed_point[0], transformed_point[1], transformed_point[2]
+p_paper_pos = untransformed_point[0], untransformed_point[1], untransformed_point[2]
 o0 = p_pos[0] - plane_basis[0]*p_paper_pos[0][0] - plane_basis[1]*p_paper_pos[0][1]
 o1 = p_pos[1] - plane_basis[0]*p_paper_pos[1][0] - plane_basis[1]*p_paper_pos[1][1]
 o2 = p_pos[2] - plane_basis[0]*p_paper_pos[2][0] - plane_basis[1]*p_paper_pos[2][1]
-print o
+print origin_transformed_paper
 print o0
 print o1
 print o2
 ###========================================#
-print "Experimental solution? I think this is what I have in the report"
+##This part tests the theory of the algorithm
+##in the case of a flat surface where it works even when varying orientation
+##(I did not expect this, but the power of LS-methods are stronger than I thought?
+print "\nSecond stage of algorithm - find delta-prim"
 import numpy
 mat = numpy.array
 
 
 Rk = []
-dprim = mat([0.1, 0, 5])
+dprim = mat([rand(), rand(), rand()])
+dprim = 100*rand()*dprim / norm(dprim)
 xprimk = []
 Xtcpk = []
 dtk = []
 Tk = []
 
-N = 24
-for k in range(0,N):
-    Rk.append( R(rand()*45, rand()*45, rand()*45) )
-    xprimk.append( mat([rand()*100, rand()*100, 0, 1]) )
+N = num_points
+for k in xrange(0,N):
+    Rk.append( rotation_matrix(rand()*360-180, rand()*180-90, rand()*360-180) )
+    xprimk.append( mat([rand()*1000, rand()*1000, 0, 1]) )
 
     Xtcp = (xprimk[-1][0:3] + Rk[-1].dot(dprim)).tolist() + [1]
     Xtcpk.append( Xtcp )
@@ -174,17 +107,17 @@ Xtcpk = mat(Xtcpk)
 dtk = mat(dtk)
 Tk = mat(Tk)
 ###
-dRk = Rk[range(1, N)] - Rk[range(0, N-1)]
+dRk = Rk[xrange(1, N)] - Rk[xrange(0, N-1)]
 
-dxprimk = xprimk[range(1, N)] - xprimk[range(0, N-1)]
+dxprimk = xprimk[xrange(1, N)] - xprimk[xrange(0, N-1)]
 ##dxprimk[:,3] = 1
 
-dXtcpk = Xtcpk[range(1, N)] - Xtcpk[range(0, N-1)]
+dXtcpk = Xtcpk[xrange(1, N)] - Xtcpk[xrange(0, N-1)]
 ##dXtcpk[:,3] = 1
 
-ddtk = dtk[range(1, N)] - dtk[range(0, N-1)]
+ddtk = dtk[xrange(1, N)] - dtk[xrange(0, N-1)]
 
-dTk = Tk[range(1, N)] - Tk[range(0, N-1)]
+dTk = Tk[xrange(1, N)] - Tk[xrange(0, N-1)]
 ##dTk[:,3,3] = 1
 
 tmp = dTk.reshape(((N-1)*4,4))
@@ -200,16 +133,18 @@ B = tmp.T.dot(B)
 
 s = numpy.linalg.solve(A,B)
 #s /= s[-1]
-print s
-print dprim
-print norm(s)
 
-print "All in all I remembr thinking these were all correct, but I will have to recheck my derivations and report."
+print "\normal_transformed_paper=== Result ==="
+print '\tdelta-prim = ' + str(dprim)
+print '\tnorm = ' + str(norm(dprim))
+print '\normal_transformed_paper\ts     = ' + str(s)
+print '\tnorm = ' + str(norm(s))
+print '\normal_transformed_paper\terror = ' + str(numpy.abs(s[0:3]-dprim))
 
 
 ###========================================#
-##ax.scatter(p[:,0], p[:,1], p[:,2])
-##ax.scatter(p_paper[:,0], p_paper[:,1], p_paper[:,2])
-##plot_plane(ax, pl)
-##plot_plane(ax, paper)
-##show()
+ax.scatter(transformed_point[:,0], transformed_point[:,1], transformed_point[:,2])
+ax.scatter(untransformed_point[:,0], untransformed_point[:,1], untransformed_point[:,2])
+plot_plane(ax, transformed_paper)
+plot_plane(ax, untransformed_paper)
+show()
--- source/helperfunctions.py
+++ source/helperfunctions.py
@@ -1,5 +1,139 @@
 from __future__ import division
-from numpy import dot
+#----------------------------------------#
+from scipy import *
+from numpy import *
+from pylab import *
+from numpy.linalg import *
+from numpy.random import rand
+from numpy import array as mat
+#----------------------------------------#
+def matmul_series(*matrix_factors):
+    '''
+        Takes a list of matrices as arguments and perform
+        a series of matrix multiplications from left to right
+        in the order given.
 
-def matmul_series(matrix_factors):
-	return reduce(dot, matrix_factors, 1)
+        The parameters may contain vectors and scalars as well as long as
+        the matrix-vector dimensions are the same.
+
+        Note:
+        The sequence can not start with a scalar, it must start
+        with either a vector or a matrix of type numpy.ndarray.
+    '''
+    return reduce(dot, matrix_factors, 1)
+#----------------------------------------#
+def deg_to_rad(x):
+    return x * pi / 180
+#----------------------------------------#
+def rotation_matrix_z(angle):
+    '''
+        creates a rotation Z-mapping from subspace to worldspace
+        using euler angles in degrees
+    '''
+    angle = deg_to_rad(angle)
+    return array([[ cos(angle), -sin(angle), 0 ],
+                  [ sin(angle),  cos(angle), 0 ],
+                  [  0   ,    0  , 1 ]])
+#----------------------------------------#
+def rotation_matrix_x(angle):
+    '''
+        creates a rotation X-mapping from subspace to worldspace
+        using euler angles in degrees
+    '''
+    angle = deg_to_rad(angle)
+    return array([[ 1,     0     ,           0 ],
+                  [ 0, cos(angle), -sin(angle) ],
+                  [ 0, sin(angle),  cos(angle)]])
+#----------------------------------------#
+def rotation_matrix(rot, tilt, skew):
+    '''
+        creates a rotation ZXZ-mapping from subspace to worldspace
+        using euler angles in degrees
+    '''
+    return matmul_series(rotation_matrix_x(rot), rotation_matrix_z(tilt), rotation_matrix_x(skew))
+#----------------------------------------#
+def orthogonal_projection_vectors(a, b):
+    '''
+        Orthogonal projection of vector a onto vector b
+    '''
+    c = b / norm(b)
+    return a.dot(c) * c
+#----------------------------------------#
+def gram_schmith_step(a, b):
+    '''
+        Calculates the orthogonal component of A relative to the
+        parallell direction vector B
+    '''
+    return a - orthogonal_projection_vectors(a, b)
+#----------------------------------------#
+def coordinate_system_from_two_directions(dirz, diry):
+    '''
+        Generates an orthogonal coordinate system from two vectors in world space,
+        the vectors themselves need not be orthogonal.
+
+        Alternative interpretation:
+        creates a (general) rotation mapping from subspace to worldspace
+        using vector directions
+    '''
+    dirz = dirz / norm(dirz)
+    diry = diry / norm(diry)
+    diry = gram_schmith_step(diry, dirz)
+    dirx = cross(diry, dirz)
+    return array([dirx, diry, dirz]).T
+#----------------------------------------#
+def convert_to_mat(*args):
+    """
+        Converts a list of arguments to numpy.ndarray,
+        the arguments are expected to be tuples or lists,
+        scalar values are also accepted but it's not the functions
+        intended use.
+    """
+    return map(mat, args)
+#----------------------------------------#
+def define_plane(origin, dirx, diry):
+    """
+        This function returns a coordinate system,
+        defined as a set containing a point of origin
+        and a set of basis vectors all of dimension N.
+        {O,{basis_x, basis_y, ...}}
+    """
+    origin, dirx, diry = convert_to_mat(origin, dirx, diry)
+    diry = gram_schmith_step(diry, dirx)
+    diry = diry / norm(diry)
+    dirx = dirx / norm(dirx)
+    normal = cross(dirx, diry)
+    return origin, dirx, diry, normal
+#----------------------------------------#
+def get_plane_point(plane,x,y):
+    """
+        From a subspace plane coordinate system with local coordinates (x,y)
+        return the coordinates in corresponding world-space.
+    """
+    origin, dx, dy, n = plane
+    pos = origin + x*dx + y*dy
+    return pos
+#----------------------------------------#
+def get_relative_point(plane,x,y,Dx,Dy,L):
+    origin, dx, dy, n = plane
+    pos = get_plane_point(plane,x,y)
+    vec = n + Dx*dx + Dy*dy
+    vec = vec / norm(vec)
+    rel = pos + L*vec
+    dir = pos - rel
+    dir = dir / norm(dir)
+    return rel, dir, pos,(dx*x,dy*y)
+#----------------------------------------#
+def init_plot():
+    from mpl_toolkits.mplot3d import Axes3D
+    import matplotlib.pyplot as plt
+    fig = plt.figure()
+    ax = fig.gca(projection='3d') 
+    return ax, fig
+#----------------------------------------#
+def plot_plane(ax, plane):
+    dx = array([get_plane_point(plane,0,0), get_plane_point(plane,1,0)])
+    dy = array([get_plane_point(plane,0,0), get_plane_point(plane,0,1)])
+    dz = array([get_plane_point(plane,0,0), get_plane_point(plane,0,0) + plane[3]])
+    ax.plot(dx[:,0],dx[:,1],dx[:,2],'b')
+    ax.plot(dy[:,0],dy[:,1],dy[:,2],'g')
+    ax.plot(dz[:,0],dz[:,1],dz[:,2],'r')
