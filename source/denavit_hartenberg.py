070b1a48fe92fc15e64ddbf31ca3bdbc34e1c833
Jonny Sjöberg <jonny.sjoberg.229@student.lu.se>
2015-06-28 16:23:24 +0200

    expanded the inverse kinematics to contain the extra 4 missing solutions

--- source/denavit_hartenberg.py
+++ source/denavit_hartenberg.py
@@ -86,7 +86,7 @@ def calc_tool_IRB120_sub(a,b,c):
                     )
     return flange
 #----------------------------------------------------------------------------------------------------------#
-def __calc_inv_kin_irb120_orientation(j1, j2, j3, T44):
+def __IK_irb120__orientation(j1, j2, j3, T44):
     #Calculate last angles
     R = T44[0:3,0:3]    
     H3,_ = calc_tool_IRB120_sub(j1, j2, j3)
@@ -99,9 +99,24 @@ def __calc_inv_kin_irb120_orientation(j1, j2, j3, T44):
     j4 = atan2(Z[1],Z[0])
     j5 = atan2(norm(Z[0:2]), Z[2])
     j6 = atan2(X[2], Y[2]) + 90
-    return j4, j5, j6
 
-def __IK_irb120_position_elbow_up(wcp, x1, alpha, beta, T44, flipped=False):
+    j41 = 0
+    j51 = 0
+    j61 = 0
+    return j4, j5, j6, j41, j51, j61
+
+def __IK_irb120_position_elbow_up(T44, flipped=False):
+    #Geometrical paramaters
+    wcp = calc_wcp(T44)
+    x0 = norm((wcp[0],wcp[1]))
+    h1 = norm(mat([0,0,290e-3]))
+    x0p = norm(mat([0,0,h1]) - mat([wcp[0],wcp[1],0]))
+    h2 = wcp[2]
+    s = abs(h2 - h1)
+    x1 = norm(mat([0,0,h1]) - wcp[0:3])
+    beta = sqrt(0.070**2 + 0.302**2)
+    alpha = 270e-3
+    m = atan(0.070 / 0.302)
     #First angle - j1
     j1 = atan2(wcp[1],wcp[0])
     if not flipped:
@@ -117,19 +132,68 @@ def __IK_irb120_position_elbow_up(wcp, x1, alpha, beta, T44, flipped=False):
         j2 = 90 - th2
     else:
         ### elbow-up (actually inverse, upside-down) ###
-        # Third angle - j3
         if j1 > 0:
             j1 = j1 - 180
         elif j1 < 0:
             j1 = j1 + 180
+        # Third angle - j3
         th3 = ang_sats2(x1, alpha, beta)
-        j3 = -(90 - th3 - m)
+        j3 = -(90 - (th3 + m))
         # Second angle - j2
         th21 = atan2(s, x0)
         th22 = atan2(beta*sin2(th3), alpha + beta*cos2(th3))
         th2 = th21 - th22
         j2 = -(90-th2)
-    return j1, j2, j3
+
+    j4, j5, j6,\
+        j41,j51,j61 = __IK_irb120__orientation(j1, j2, j3, T44)
+    return (j1, j2, j3, j4, j5, j6), (j1, j2, j3, j41, j51, j61)
+
+def __IK_irb120_position_elbow_down(T44, flipped=False):
+    #Geometrical paramaters
+    wcp = calc_wcp(T44)
+    x0 = norm((wcp[0],wcp[1]))
+    h1 = norm(mat([0,0,290e-3]))
+    x0p = norm(mat([0,0,h1]) - mat([wcp[0],wcp[1],0]))
+    h2 = wcp[2]
+    s = abs(h2 - h1)
+    x1 = norm(mat([0,0,h1]) - wcp[0:3])
+    beta = sqrt(0.070**2 + 0.302**2)
+    alpha = 270e-3
+    m = atan(0.070 / 0.302)
+    #First angle - j1
+    j1 = atan2(wcp[1],wcp[0])
+    if not flipped:
+        ### elbow-down ###
+        # Third angle - j3
+        th3 = ang_sats2(x1, alpha, beta)
+        j3 = -(th3 - m + 90)
+        # Second angle - j2
+        th21 = atan2(s, x0)
+        th22 = atan2(beta*sin2(th3), alpha + beta*cos2(th3))
+        th2 =  th21 - th22
+        j2 = 90 - th2
+    else:
+        ### elbow-down (actually inverse, upside-down) ###
+        if j1 > 0:
+            j1 = j1 - 180
+        elif j1 < 0:
+            j1 = j1 + 180
+        # Third angle - j3
+        th3 = ang_sats2(x1, alpha, beta)
+        j3 = -(th3 - m + 90)
+        # Second angle - j2
+        th21 = atan2(s, x0)
+        th22 = atan2(beta*sin2(th3), alpha + beta*cos2(th3))
+        th2 =  th21 + th22
+        j2 = -(90 - th2)
+
+    j4, j5, j6,\
+        j41,j51,j61 = __IK_irb120__orientation(j1, j2, j3, T44)
+    return (j1, j2, j3, j4, j5, j6), (j1, j2, j3, j41, j51, j61)
+    
+def calc_wcp(T44):
+    return (T44[:,3] - T44[:,2]*0.072)[0:3]
 
 def calc_inv_kin_IRB120(T44):
     if type(T44) is list:
@@ -142,55 +206,13 @@ def calc_inv_kin_IRB120(T44):
     if dim[0] != 4:
         raise ArithmeticError('Forward-kinematics must have dimension of 4!')
 
-    #INVERSE KINEMATICS STARTS HERE
-    p_end = T44[0:3,3]
-    wcp = (T44[:,3] - T44[:,2]*0.072)[0:3]
-        
-    #Geometrical paramaters
-    x0 = norm((wcp[0],wcp[1]))
-    h1 = norm(mat([0,0,290e-3]))
-    x0p = norm(mat([0,0,h1]) - mat([wcp[0],wcp[1],0]))
-    h2 = wcp[2]
-    s = abs(h2 - h1)
-    x1 = norm(mat([0,0,h1]) - wcp[0:3])
-    beta = sqrt(0.070**2 + 0.302**2)
-    alpha = 270e-3
-    m = atan(0.070 / 0.302)
+    sol1, sol11 = __IK_irb120_position_elbow_up(T44)
+    sol2, sol21 = __IK_irb120_position_elbow_down(T44)
+    sol3, sol31 = __IK_irb120_position_elbow_up(T44, flipped = True)
+    sol4, sol41 = __IK_irb120_position_elbow_down(T44, flipped = True)
 
-    sol1 = ()
-
-
-    ### elbow-down ###
-    # Third angle - j3
-    th3 = ang_sats2(x1, alpha, beta)
-    gamma2 = -(th3 - m + 90)
-    print 'c-norm: ' + str(norm( gamma2-c ))
-    # Second angle - j2
-    th21 = atan2(s, x0)
-    th22 = atan2(beta*sin2(th3), alpha + beta*cos2(th3))
-    th2 =  th21 - th22
-    gamma1 = 90 - th2
-    print 'b-norm: ' + str(norm( b - gamma1 ))
-
-    ### elbow-down (actually inverse, upside-down) ###
-    # Third angle - j3
-    if gamma0 > 0:
-        gamma0 = gamma0 - 180
-    elif gamma0 < 0:
-        gamma0 = gamma0 + 180
-    th3 = ang_sats2(x1, alpha, beta)
-    gamma2 = -(th3 - m + 90)
-    # Second angle - j2
-    print 'c-norm: ' + str(norm( gamma2-c ))
-    th21 = atan2(s, x0)
-    th22 = atan2(beta*sin2(th3), alpha + beta*cos2(th3))
-    th2 =  th21 + th22
-    gamma1 = -(90 - th2)
-    print 'b-norm: ' + str(norm( b - gamma1 ))
-
-    
-
-    
+    #first columnt is first solution and so forth
+    return zip(sol1, sol2, sol3, sol4, sol11, sol21, sol31, sol41)
 #----------------------------------------------------------------------------------------------------------#
 def custom_round(v, prec = 1e-8):
     coef = 1 / prec
@@ -252,101 +274,44 @@ if __name__ == '__main__':
 
     #INVERSE KINEMATICS STARTS HERE
     p_end = T44[0:3,3]
-    wcp = (T44[:,3] - T44[:,2]*0.072)[0:3]
-        
-    print "wcp-norm: "+str(norm(data['Joint_6_T'][index][0:3,3] - wcp))
-    x0 = norm((wcp[0],wcp[1]))
+    wcp = calc_wcp(T44)
     
-    #Geometrical parameters
-    h1 = norm(mat([0,0,290e-3]))
-    x0p = norm(mat([0,0,h1]) - mat([wcp[0],wcp[1],0]))
-    h2 = wcp[2]
-    s = abs(h2 - h1)
-    x1 = norm(mat([0,0,h1]) - wcp[0:3])
-    beta = sqrt(0.070**2 + 0.302**2)
-    alpha = 270e-3
-    m = atan(0.070 / 0.302)
+    sol = mat( calc_inv_kin_IRB120(T44) )
+    s0 = mat([a,b,c,d,e,f])
+    for i in xrange(0, 8):
+        s = sol[:,i]
+        gamma0,gamma1,gamma2,gamma3,gamma4,gamma5 = s
+        A, debug = calc_tool_IRB120(gamma0,gamma1,gamma2,gamma3,gamma4,gamma5)
+        p0 = debug[0][:,3]
+        p1 = matmul(debug[0],debug[1])[:,3]
+        p2 = matmul(debug[0],debug[1],debug[2])[:,3]
+        p3 = matmul(debug[0],debug[1],debug[2], debug[3])[:,3]
+        p4 = matmul(debug[0],debug[1],debug[2], debug[3], debug[4])[:,3]
+        p5 = matmul(debug[0],debug[1],debug[2], debug[3], debug[4], debug[5])[:,3]
+        print "\n[ Solution %s ]" % str(i)
+        print "FK-norm: " + str( norm(A - T44) )
+        print "angle-norm: %0.2f" % norm(s - s0)
 
-    #First angle - j1
-    gamma0 = atan2(wcp[1],wcp[0])
-    print 'a-norm: ' + str( norm(a - gamma0 ))
-
-
-##    ### elbow-up ###
-    gamma0, gamma1, gamma2 = __IK_irb120_position_elbow_up(wcp, x1, alpha, beta, T44)
-
-
-##    ### elbow-up (actually inverse, upside-down) ###
-##    ### elbow-up ###
-    gamma0, gamma1, gamma2 = __IK_irb120_position_elbow_up(wcp, x1, alpha, beta, T44, True)
-
-##    ### elbow-down ###
-##    # Third angle - j3
-##    th3 = ang_sats2(x1, alpha, beta)
-##    gamma2 = -(th3 - m + 90)
-##    print 'c-norm: ' + str(norm( gamma2-c ))
-##    # Second angle - j2
-##    th21 = atan2(s, x0)
-##    th22 = atan2(beta*sin2(th3), alpha + beta*cos2(th3))
-##    th2 =  th21 - th22
-##    gamma1 = 90 - th2
-##    print 'b-norm: ' + str(norm( b - gamma1 ))
-
-##    ### elbow-down (actually inverse, upside-down) ###
-##    # Third angle - j3
-##    if gamma0 > 0:
-##        gamma0 = gamma0 - 180
-##    elif gamma0 < 0:
-##        gamma0 = gamma0 + 180
-##    th3 = ang_sats2(x1, alpha, beta)
-##    gamma2 = -(th3 - m + 90)
-##    # Second angle - j2
-##    print 'c-norm: ' + str(norm( gamma2-c ))
-##    th21 = atan2(s, x0)
-##    th22 = atan2(beta*sin2(th3), alpha + beta*cos2(th3))
-##    th2 =  th21 + th22
-##    gamma1 = -(90 - th2)
-##    print 'b-norm: ' + str(norm( b - gamma1 ))
-
-##    #Calculate last angles
-##    R = T44[0:3,0:3]    
-##    H3,_ = calc_tool_IRB120_sub(gamma0, gamma1, gamma2)
-##    R3 = H3[0:3, 0:3]
-##    R36 = R3.T.dot(R)
-##    X = R36[:,0]
-##    Y = R36[:,1]
-##    Z = R36[:,2]
-##    # for order of parameters check numpy.info(numpy.arctan2)
-##    gamma3 = atan2(Z[1],Z[0])
-##    print 'd-norm: ' + str(norm( gamma3-d ))
-##
-##    # for order of parameters check numpy.info(numpy.arctan2)
-##    gamma4 = atan2(norm(Z[0:2]), Z[2])
-##    print 'e-norm: ' + str(norm( gamma4-e ))
-##
-##    gamma5 = atan2(X[2], Y[2]) + 90
-##    print 'f-norm: ' + str(norm( gamma5-f ))
-    gamma3, gamma4, gamma5 = __calc_inv_kin_irb120_orientation(gamma0, gamma1, gamma2, T44)
-    
-    
-
-    A, debug = calc_tool_IRB120(gamma0,gamma1,gamma2,gamma3,gamma4,gamma5)
-    p0 = debug[0][:,3]
-    p1 = matmul(debug[0],debug[1])[:,3]
-    p2 = matmul(debug[0],debug[1],debug[2])[:,3]
-    p3 = matmul(debug[0],debug[1],debug[2], debug[3])[:,3]
-    p4 = matmul(debug[0],debug[1],debug[2], debug[3], debug[4])[:,3]
-    p5 = matmul(debug[0],debug[1],debug[2], debug[3], debug[4], debug[5])[:,3]
-
-    print "FK-norm: " + str(norm(A - T44))
-
-    #Plotting
-    from pylab import plot, show
-    M = mat(zip([0,0,0],p0,p1,p2,p3,p4,p5)).T
+        #Plotting
+        from pylab import plot, show, legend
+        M = mat(zip([0,0,0],p0,p1,p2,p3,p4,p5)).T
+        if i == 0:
+            col = 'b-'
+            lw = 3
+        if i == 1:
+            col = 'r-'
+            lw = 3
+        if i == 2:
+            col = 'b-.'
+            lw = 2
+        if i == 3:
+            col = 'r-.'
+            lw = 2
+        plot(M[:,0], M[:,2], col, linewidth= lw)
+        legend(['elbow-up', 'elbow-down', 'elbow-up-flipped', 'elbow-down-flipped'])
+    plot([-1,-1,1,1],[0,1,1,0],'w')
     plot(wcp[0], wcp[2], 'ro')
     plot(p_end[0], p_end[2], 'ko')
-    plot(M[:,0],M[:,2])
-    plot([-1,-1,1,1],[0,1,1,0],'w')
     show()
 
 ###############################################################
