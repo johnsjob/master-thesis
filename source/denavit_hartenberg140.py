8240bcb14ffe7d3c93ca71ddfa316cbe246ee833
Jonny Sjöberg <jonny.sjoberg.229@student.lu.se>
2015-07-31 21:33:23 +0200

    altered the test-case for inverse kinematics for IRB140

--- source/denavit_hartenberg140.py
+++ source/denavit_hartenberg140.py
@@ -86,7 +86,6 @@ def inverse_kinematics_elbow_up(dh_table, T44, flipped=False):
         th22 = atan2(beta*sin2(th3), alpha + beta*cos2(th3))
         th2 = th21 - th22
         j2 = -(90-th2)
-
     j4, j5, j6,\
         j41,j51,j61 = inverse_kinematics_spherical_wrist(dh_table, j1, j2, j3, T44)
     return (j1, j2, j3, j4, j5, j6), (j1, j2, j3, j41, j51, j61)
@@ -171,13 +170,14 @@ def inverse_kinematics_irb140(dh_table, T44):
     if dim[0] != 4:
         raise ArithmeticError('Forward-kinematics must have dimension of 4!')
 
-    sol1, sol11 = inverse_kinematics_elbow_up(dh_table, T44)
-    sol2, sol21 = inverse_kinematics_elbow_down(dh_table, T44)
-    sol3, sol31 = inverse_kinematics_elbow_up(dh_table, T44, flipped = True)
-    sol4, sol41 = inverse_kinematics_elbow_down(dh_table, T44, flipped = True)
+    sol_elbup1,      sol_elbup2      = inverse_kinematics_elbow_up(dh_table, T44)
+    sol_elbdown1,    sol_elbdown2    = inverse_kinematics_elbow_down(dh_table, T44)
+    sol_elbup1_fl,   sol_elbup2_fl   = inverse_kinematics_elbow_up(dh_table, T44, flipped = True)
+    sol_elbdown1_fl, sol_elbdown2_fl = inverse_kinematics_elbow_down(dh_table, T44, flipped = True)
 
     #first columnt is first solution and so forth
-    return mat(zip(sol1, sol2, sol3, sol4, sol11, sol21, sol31, sol41))
+    return mat(zip(sol_elbup1, sol_elbdown1, sol_elbup1_fl, sol_elbdown1_fl,
+                   sol_elbup2, sol_elbdown2, sol_elbup2_fl, sol_elbdown2_fl))
 
 def filter_solutions(solutions, filter_function = check_solution):
     result = []
@@ -221,6 +221,7 @@ class TestIRB140(unittest.TestCase):
         self.joint_values = zip(a,b,c,d,e,f)
 
     def test_forward_kinematics(self):
+        print '### TEST-forward-kinematics-from-file\n'
         data = self.data
         num_joint_conf = self.num_joint_conf
         for index in xrange(num_joint_conf):
@@ -238,7 +239,10 @@ class TestIRB140(unittest.TestCase):
             self.assertLess(norm(T44 - A), 1e-7)
         
     def test_inverse_kinematics(self):
+        print '### TEST-inverse-kinematics-from-file'
         nans = 0
+        total_valid_sols = 0
+        num_zeroed_angles = 0
         for index in xrange(self.num_joint_conf):
             T44 = self.T44[index]
             sol = mat( inverse_kinematics_irb140(DH_TABLE, T44) )
@@ -248,8 +252,14 @@ class TestIRB140(unittest.TestCase):
             print "\n[ IK %s ]" % str(index)
 
             num_zeroed_angle_norms = 0
+            num_valid_solutions = 0
             for i in xrange(0, 8):
+                solution_names = ['sol_elbup1', 'sol_elbdown1', 'sol_elbup1_fl', 'sol_elbdown1_fl',
+                                  'sol_elbup2', 'sol_elbdown2', 'sol_elbup2_fl', 'sol_elbdown2_fl']
                 s = sol[:,i]
+                is_valid_solution = check_solution(*s)
+                num_valid_solutions += is_valid_solution
+                
                 gamma0,gamma1,gamma2,gamma3,gamma4,gamma5 = s
                 A, debug = forward_kinematics(gamma0, gamma1, gamma2,
                                                  gamma3, gamma4, gamma5, **DH_TABLE)
@@ -261,106 +271,119 @@ class TestIRB140(unittest.TestCase):
                 p5 = matmul(debug[0],debug[1],debug[2], debug[3], debug[4], debug[5])[:,3]
                 fk_norm = norm(A - T44)
                 angle_norm = norm(s - s0)
-                print "Solution: %s" % str(i)
-                print "FK-norm: " + str(fk_norm)
-                if n.isnan(fk_norm):
+
+                print "\n\tSolution: %s" % solution_names[i]
+                print "\tFK-norm: " + str(fk_norm)
+                if not n.isnan(fk_norm):
+                    self.assertLess(fk_norm, 1e-14)
+                else:
                     nans += 1
-                    print A
-                    print T44
-                    print s
-                    print s0
-                    continue
-                self.assertLess(fk_norm, 1e-14)
-                print "angle-norm: %s" % str(angle_norm)
-                if angle_norm < 1e-8:
-                    num_zeroed_angle_norms += 1
-                elif int(angle_norm) == 360:
-                    num_zeroed_angle_norms += 1
                     
-                elif angle_norm < 1:
-                    print str(s - s0)
-            self.assertGreaterEqual(num_zeroed_angle_norms, 1)
+                print "\tangle-norm: %s" % str(angle_norm)
+                if not n.isnan(fk_norm):
+                    if angle_norm < 1e-8:
+                        num_zeroed_angle_norms += 1
+                    elif int(angle_norm*1000) == 360000:
+                        num_zeroed_angle_norms += 1
+                    elif angle_norm < 1:
+                        print str(s - s0)
+            self.assertGreater(num_zeroed_angle_norms, 0)
+
+            print "\n\tnum valid solutions: %s" % str(num_valid_solutions)
+            print "\tnum angle norms ~0: %s" % str(num_zeroed_angle_norms)
+            num_zeroed_angles += num_zeroed_angle_norms
+            total_valid_sols += num_valid_solutions
+            self.assertGreater(num_valid_solutions, 0)
         print 'nans = '+str(nans)
-        self.assertEqual(nans, 0)
+        print 'total_valid = '+str(total_valid_sols)
+        print 'total_possible = '+str(self.num_joint_conf*8)
+        print 'total_zeroed_angles = '+str(num_zeroed_angles)
 #----------------------------------------------------------------------------------------------------------#
-
 if __name__ == '__main__':
-#    unittest.main()
-    """
-    GENERAL COMMENTS:
-    ################
-        data['Joint_(i)_T'] = cumulative multiplication of up to i-1 of joint T-matrices
-
-        'debug' contains i:th matrices of python implementation of DH-algorithm
-
-        lambda function up_to(i) performs cumulative multiplication up to i:th joint
-
-        wcp = data['Joint_6_T'][index] = up_to(5)
-    """
-    clear()
-    index = -1
-
-    data = parse.parse_file("C:\\robot-studio-output.txt")
-    num_joint_conf = len(data['Joint_1_T'])
-            
-            
-    params = zip(data['A'], data['alpha'], data['D'], data['theta'])
-    T44 = mat(data['T44'][index]).reshape((4,4))
-
-    for key in data:
-        if 'T' in key:
-            data[key] = custom_round(mat(data[key]).reshape(num_joint_conf,4,4))
-
-    print "\nNumber of configurations: " + str(len(data['Joint_1'])) + "\n"
-    a,b,c,d,e,f = data['Joint_1'][index], data['Joint_2'][index], data['Joint_3'][index], data['Joint_4'][index], data['Joint_5'][index], data['Joint_6'][index],
-
-    A, debug  = forward_kinematics(a,b,c,d,e,f, **DH_TABLE)
-    #calc_tool(DH, a,b,c,d,e,f)
-
-    print "T44 sanity check-norm: " + str(norm(T44 - A))
-
-    p_end = T44[0:3,3]
-    wcp = calc_wcp(T44, 0.065)
-    sol = mat( inverse_kinematics_irb140(DH_TABLE, T44) )
-    s0 = mat([a,b,c,d,e,f])
-    all_norms = 0
-    for i in xrange(0, 8):
-        s = sol[:,i]
-        gamma0,gamma1,gamma2,gamma3,gamma4,gamma5 = s
-        A, debug = forward_kinematics(gamma0, gamma1, gamma2,
-                                         gamma3, gamma4, gamma5, **DH_TABLE)
-        p0 = debug[0][:,3]
-        p1 = matmul(debug[0],debug[1])[:,3]
-        p2 = matmul(debug[0],debug[1],debug[2])[:,3]
-        p3 = matmul(debug[0],debug[1],debug[2], debug[3])[:,3]
-        p4 = matmul(debug[0],debug[1],debug[2], debug[3], debug[4])[:,3]
-        p5 = matmul(debug[0],debug[1],debug[2], debug[3], debug[4], debug[5])[:,3]
-        print "\n[ Solution %s ]" % str(i)
-        print "FK-norm: " + str( norm(A - T44) )
-        all_norms = all_norms + norm(A - T44)
-        print "angle-norm: %0.2f" % norm(s - s0)
-
-        #Plotting
-        from pylab import plot, show, legend
-        M = mat(zip([0,0,0],p0,p1,p2,p3,p4,p5)).T
-        if (i % 4) == 0:
-            col = 'b-'
-            lw = 3
-        if (i % 4) == 1:
-            col = 'r-'
-            lw = 3
-        if (i % 4) == 2:
-            col = 'b-.'
-            lw = 2
-        if (i % 4) == 3:
-            col = 'r-.'
-            lw = 2
-        plot(M[:,0], M[:,2], col, linewidth = lw)
-        legend(['elbow-up', 'elbow-down', 'elbow-up-flipped', 'elbow-down-flipped',
-                'elbow-up-2', 'elbow-down-2', 'elbow-up-flipped-2', 'elbow-down-flipped-2'])
-
-    print "FK-norm-summary: " + str( all_norms )
-    plot([-1,-1,1,1],[0,1,1,0],'w')
-    plot(wcp[0], wcp[2], 'ro')
-    plot(p_end[0], p_end[2], 'ko')
-    show()
+    unittest.main()
+##    """
+##    GENERAL COMMENTS:
+##    ################
+##        data['Joint_(i)_T'] = cumulative multiplication of up to i-1 of joint T-matrices
+##
+##        'debug' contains i:th matrices of python implementation of DH-algorithm
+##
+##        lambda function up_to(i) performs cumulative multiplication up to i:th joint
+##
+##        wcp = data['Joint_6_T'][index] = up_to(5)
+##    """
+##    clear()
+##    index = -1
+##
+##    data = parse.parse_file("C:\\robot-studio-output.txt")
+##    num_joint_conf = len(data['Joint_1_T'])
+##            
+##            
+##    params = zip(data['A'], data['alpha'], data['D'], data['theta'])
+##    T44 = mat(data['T44'][index]).reshape((4,4))
+##
+##    for key in data:
+##        if 'T' in key:
+##            data[key] = custom_round(mat(data[key]).reshape(num_joint_conf,4,4))
+##
+##    print "\nNumber of configurations: " + str(len(data['Joint_1'])) + "\n"
+##    a,b,c,d,e,f = data['Joint_1'][index], data['Joint_2'][index], data['Joint_3'][index], data['Joint_4'][index], data['Joint_5'][index], data['Joint_6'][index],
+##
+##    A, debug  = forward_kinematics(a,b,c,d,e,f, **DH_TABLE)
+##
+##    print "T44 sanity check-norm: " + str(norm(T44 - A))
+##
+##    p_end = T44[0:3,3]
+##    wcp = calc_wcp(T44, 0.065)
+##    sol = mat( inverse_kinematics_irb140(DH_TABLE, T44) )
+##    s0 = mat([a,b,c,d,e,f])
+##    all_norms = 0
+##
+##    solution_names = ['sol_elbup1', 'sol_elbdown1', 'sol_elbup1_fl', 'sol_elbdown1_fl',
+##                      'sol_elbup2', 'sol_elbdown2', 'sol_elbup2_fl', 'sol_elbdown2_fl']
+##
+##    num_valid_solutions = 0
+##    for i in xrange(0, 8):
+##        s = sol[:,i]
+##        num_valid_solutions += check_solution(*s)
+##        print check_solution(*s)
+##        print s
+##        gamma0,gamma1,gamma2,gamma3,gamma4,gamma5 = s
+##        A, debug = forward_kinematics(gamma0, gamma1, gamma2,
+##                                         gamma3, gamma4, gamma5, **DH_TABLE)
+##        p0 = debug[0][:,3]
+##        p1 = matmul(debug[0],debug[1])[:,3]
+##        p2 = matmul(debug[0],debug[1],debug[2])[:,3]
+##        p3 = matmul(debug[0],debug[1],debug[2], debug[3])[:,3]
+##        p4 = matmul(debug[0],debug[1],debug[2], debug[3], debug[4])[:,3]
+##        p5 = matmul(debug[0],debug[1],debug[2], debug[3], debug[4], debug[5])[:,3]
+##
+##        print "\n\t[ Solution %s ]" % solution_names[i]
+##        print "\tFK-norm: " + str( norm(A - T44) )
+##        all_norms = all_norms + norm(A - T44)
+##        print "\tangle-norm: %0.2f" % norm(s - s0)
+##
+##        #Plotting
+##        from pylab import plot, show, legend
+##        M = mat(zip([0,0,0],p0,p1,p2,p3,p4,p5)).T
+##        if (i % 4) == 0:
+##            col = 'b-'
+##            lw = 3
+##        if (i % 4) == 1:
+##            col = 'r-'
+##            lw = 3
+##        if (i % 4) == 2:
+##            col = 'b-.'
+##            lw = 2
+##        if (i % 4) == 3:
+##            col = 'r-.'
+##            lw = 2
+##        plot(M[:,0], M[:,2], col, linewidth = lw)
+##        legend(['elbow-up', 'elbow-down', 'elbow-up-flipped', 'elbow-down-flipped',
+##                'elbow-up-2', 'elbow-down-2', 'elbow-up-flipped-2', 'elbow-down-flipped-2'])
+##    print "FK-norm-summary: " + str( all_norms )
+##    print "Num valid solutions: " + str( num_valid_solutions )
+##    plot([-1,-1,1,1],[0,1,1,0],'w')
+##    plot(wcp[0], wcp[2], 'ro')
+##    plot(p_end[0], p_end[2], 'ko')
+##    show()
