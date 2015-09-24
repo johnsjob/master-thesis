a41424b9104e61a6981d066bb9f09a14027183b4
Jonny Sjöberg <jonny.sjoberg.229@student.lu.se>
2015-09-24 21:01:24 +0200

    checked how relative plane transformations are behaving (they are working as intended) and change the geometry setup accordingly in calibration algorithm and now it wors - continue with refactoring calibration algorithm

--- source/calibration_algorithm4.py
+++ source/calibration_algorithm4.py
@@ -3,8 +3,9 @@ from __future__ import division
 #----------------------------------------#
 import numpy
 import time
-mat = numpy.array
+
 from numpy.linalg import solve, det, inv, cond
+from numpy import array as mat, log10
 #----------------------------------------
 # custom imports
 from helperfunctions_math import *
@@ -12,7 +13,7 @@ from helperfunctions_plot import *
 import plane_relative as plane_tools
 #----------------------------------------#
 # num_points = 12 absolute minimum, actually 12+1
-num_points = 120
+num_points = 160
 #========================================#
 # placing paper origin
 o = mat([1000*rand(), 1000*rand(), 1000*rand()])
@@ -21,18 +22,15 @@ o = mat([1000*rand(), 1000*rand(), 1000*rand()])
 r,t,s = -rand_range(-180,180), rand_range(-180,180), rand_range(-180, 180)
 
 # define the paper-orientation in global (robot-system) directions
+# however, the paper uses "euclidean" orientation ('local' = euclidean)
 plane = plane_tools.define_plane_from_angles(o, r, t, s, 'local')
-dirx = plane[:3,0]
-diry = plane[:3,1]
-
-# Orientation of the tool in tool (local) coordinate system 
-local_tool_orientation = rotation_matrix_rot_tilt_skew(-10, 20, 30)
 #################################################
-# define delta vector which
-# we want to find (in tool-space / local space)
+# define delta vector which we want to find (in tool-space / local space)
 L = 100
 local_delta_vector = mat([1,2,3])
 local_delta_vector = (local_delta_vector / norm(local_delta_vector))*L #length L
+# Orientation of the tool in tool (local) coordinate system 
+local_tool_orientation = rotation_matrix_rot_tilt_skew(-10, 20, 30)
 #----------------------------------------
 def merge_dicts(*list_of_dicts):
     ret = {}
@@ -84,7 +82,9 @@ def generate_random_Anoto_Point(L):
     py = L*rand()-L/2.0
     return px, py
 #----------------------------------------
-def solve_tool0_tip_alt(array_forward_kinematics_T44, array_anoto2D, array_lhs_sys_eq = None):
+def solve_tool0_tip_alt(array_forward_kinematics_T44,
+                        array_anoto2D,
+                        array_lhs_sys_eq = None):
     try:
         num_points,m,n = array_forward_kinematics_T44.shape
     except Exception as e:
@@ -126,23 +126,32 @@ def solve_tool0_tip_alt(array_forward_kinematics_T44, array_anoto2D, array_lhs_s
     Z = cross(X, Y)
     result = mat([X,Y,Z,D]).T
     condition = cond(L)
+
     return result, condition
 #----------------------------------------
 def generate_Xflange_orientation(plane,rot, tilt, skew):
     """
-        Generate Xtcp-orientation in world coordinates (Rrel)
+        Generate Xtcp-orientation in world coordinates, using Anoto-paper
+        orientation formulation
         Planes are homoenous matrices, if we want the orientation
         we need the 0:3,0:3 submatrix.
     """
     return plane_tools.define_plane_relative_from_angles(plane, (0,0,0),
                                                          rot, tilt, skew,'global')[:3,:3]
 #----------------------------------------
-def setup_geometry(current_plane=None, point_spread=300, num_points=120):
-    global local_delta_vector
+def setup_geometry(current_plane, point_spread, num_points):
+    global local_delta_vector, local_tool_orientation
 
-    collected_info = {'plane':current_plane}
-    collected_data = []
+    geometry_info = {'plane':current_plane}
+    geometry_info['local_tool_orientation'] = local_tool_orientation
+    geometry_info['local_delta_vector'] = local_delta_vector
+
+    geometry_info['correct_solution_geometry'] = mat(list(geometry_info['plane'][:3,:3].T.flatten()) +
+               list(geometry_info['local_delta_vector'])).reshape(4,3).T
+
+
     #generating points and "forward-kinematics"
+    collected_data = []
     for k in xrange(0,num_points):
         info = {}
         info['angles'] = \
@@ -153,13 +162,13 @@ def setup_geometry(current_plane=None, point_spread=300, num_points=120):
         }
         # Xtcp (flange) orientation in global space, generated relative to the paper plane
         info['Xflange_orientation_relative_to_paper_plane'] = \
-                                                generate_Xflange_orientation(collected_info['plane'],**info['angles'])
+                                                generate_Xflange_orientation(geometry_info['plane'],**info['angles'])
         #generate pen-tip position in Anoto2d in mm
         px,py = generate_random_Anoto_Point(point_spread)
         info['pentip_2d'] = [px,py]
 
         # generate global Xtcp position in mm
-        info['Xtcp0'] = (plane_tools.get_plane_point(plane, px, py)[:3] - \
+        info['Xtcp0'] = (plane_tools.get_plane_point(geometry_info['plane'], px, py)[:3] - \
                  matmul( info['Xflange_orientation_relative_to_paper_plane'], local_delta_vector[:3]) )
         # ^OK
 
@@ -172,72 +181,65 @@ def setup_geometry(current_plane=None, point_spread=300, num_points=120):
                                                         info['Xtcp0'] )
         # ^OK
         collected_data.append(info)
-    collected_info['data'] = merge_dicts(*collected_data)
-    return collected_info
+    geometry_info['data'] = merge_dicts(*collected_data)
+    geometry_info['data']['angles'] = merge_dicts(*geometry_info['data']['angles'])
+
+    return geometry_info
 #----------------------------------------
-def find_solution(collected_info):
+def find_solution(geometry_info, included_solutions_from_start = -1):
     start_time = time.clock()
 
-    result, cond_num = solve_tool0_tip_alt(collected_info['data']['forward_kinematics'],
-                                      collected_info['data']['pentip_2d'],
+    result, cond_num = solve_tool0_tip_alt(geometry_info['data']['forward_kinematics'][:included_solutions_from_start],
+                                      geometry_info['data']['pentip_2d'][:included_solutions_from_start],
                                       problem_formulation)
-    stop_time = time.clock()
-    time_spent = stop_time - start_time
-
 
     # solve for orientation s which should be same as local_tool_orientation
-    flange_orientation_reshaped = collected_info['data']['Xflange_orientation_relative_to_paper_plane'].reshape(360,3)
+    l,m,n = geometry_info['data']['Xflange_orientation_relative_to_paper_plane'].shape
+    flange_orientation_reshaped = geometry_info['data']['Xflange_orientation_relative_to_paper_plane'].reshape(l*m,n)
 
     lhs = matmul( flange_orientation_reshaped.T,
                   flange_orientation_reshaped)
 
+    l,m,n = geometry_info['data']['global_tool_orientation'].shape
     rhs = matmul( flange_orientation_reshaped.T,
-                  collected_info['data']['global_tool_orientation'].reshape(360,3))
+                  geometry_info['data']['global_tool_orientation'].reshape(l*m,n))
 
     solved_tool_orientation = linalg.solve(lhs, rhs)
 
+    stop_time = time.clock()
+    time_spent = stop_time - start_time
+
     return result, solved_tool_orientation, cond_num, time_spent
 #----------------------------------------
-if __name__ == '__main__':
-    print "Sampling points..."    
-    collected_info = setup_geometry(plane)
-
-    print "Solving for dirx, diry, local_delta_vector..."
-    result, s, cond_num, time_spent = find_solution(collected_info)
-
-    print
-    print 'Time spent solving '+str(num_points)+' points: ' + str(time_spent) +' seconds.'
-
-    print
-    print 'Preparing plots...'
-    comp = mat(list(plane[:3,:3].T.flatten()) +
-               list(local_delta_vector)).reshape(4,3).T
-    l_cond = []
-    l_err = []
-    for k in xrange(3,num_points):
-        res, cond_num = solve_tool0_tip_alt(collected_info['data']['forward_kinematics'][0:k,:,:],
-                                      collected_info['data']['pentip_2d'][0:k],
-                                      problem_formulation)
-        l_cond.append(cond_num)
-
-        err = abs(comp-res)
-        l_err.append(norm(err))    
-    print 'solution error = ' + str(norm(err))
-
-    t = range(3,num_points)
-    logcond = numpy.log10(l_cond)
-    logerr = numpy.log10(l_err)
-    plot(t, logcond, label='Condition number',linewidth=2);
-    plot(t, logerr, label='Error (frobenious norm)',linewidth=2);
-    hlines(-1, t[0], t[-1], label='Tolerance = 10^-1')
-    xlim(t[0], t[-1])
+def perform_solution_run(geometry_info):
+    interval = range(3,num_points)
+    list_of_solving = []
+    for k in interval:
+        solve_info = {}
+        solve_info['result'], solve_info['solved_tool_orientation'], solve_info['cond_num'], solve_info['time_spent'] = \
+                                find_solution(geometry_info, k)                    
+        solve_info['err'] = norm(geometry_info['correct_solution_geometry'] - solve_info['result']) + norm(geometry_info['local_tool_orientation'] - solve_info['solved_tool_orientation'])
+        list_of_solving.append(solve_info)
+    solving_data = merge_dicts(*list_of_solving)
+    solving_data['interval'] = interval
+    print 'solution error = ' + str( solving_data['err'][-1] )
+    return solving_data
+#----------------------------------------
+def make_plots(solving_data):
+    logcond = log10( solving_data['cond_num'] )
+    logerr  = log10( solving_data['err'] )
+    plot(solving_data['interval'], logcond, label='Condition number',       linewidth=2);
+    plot(solving_data['interval'], logerr,  label='Error (frobenious norm)',linewidth=2);
+    plot(solving_data['interval'], log10( solving_data['time_spent'] ),'r', label='time spent',linewidth=2)
+    hlines(-1, solving_data['interval'][0], solving_data['interval'][-1], label='Tolerance = 10^-1')
+    xlim(solving_data['interval'][0], solving_data['interval'][-1])
     xlabel('Number of points collected', fontsize=14)
     ylabel('log10', fontsize=14)
 
     index = 4-3
     plt.annotate("number of points = 4",
-                xy=(t[index]+0.01, logerr[index]+0.2), xycoords='data',
-                xytext=(t[index]+0.8, logerr[index]+4.5), textcoords='data',
+                xy=(solving_data['interval'][index]+0.01, logerr[index]+0.2), xycoords='data',
+                xytext=(solving_data['interval'][index]+0.8, logerr[index]+4.5), textcoords='data',
                 arrowprops=dict(arrowstyle="->",
                                 connectionstyle="arc3"),
                 )
@@ -245,3 +247,21 @@ if __name__ == '__main__':
     title('Calibration algorithm verification using simulated geometry')
     legend()
     show()
+#----------------------------------------
+if __name__ == '__main__':
+    print "Sampling points..."    
+    geometry_info = setup_geometry(plane, 300, num_points)
+
+    print "Solving for dirx, diry, dirz and local_delta_vector..."
+    result, s, cond_num, time_spent = find_solution(geometry_info)
+
+    print
+    print 'Time spent solving '+str(num_points)+' points: ' + str(time_spent) +' seconds.'
+
+    print
+    print 'Collecting solving information...'
+    solving_data = perform_solution_run(geometry_info)
+
+    print
+    print 'Preparing plots...'
+    make_plots(solving_data)
