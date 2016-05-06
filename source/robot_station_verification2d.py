f626fca665204aac26ba8c1a1a5412050ac8169f
Jonny Sjöberg <jonny.sjoberg.229@student.lu.se>
2016-05-06 15:16:28 +0200

    refactored robot_Station2d so that it

--- source/robot_station_verification2d.py
+++ source/robot_station_verification2d.py
@@ -1,7 +1,10 @@
 import random
 
-from pylab import axhline, plot, show, axes
-from helperfunctions_math import mat
+from pylab import axhline, plot, show, axes, grid
+
+from helperfunctions_math import mat, homogenous_matrix as hom, nzip,\
+     rotation_matrix_skew_tilt_rot as ori
+
 from denavit_hartenberg140 import forward_kinematics,\
      calc_valid_raw_invkin_irb140,\
      calc_valid_invkin_irb140,\
@@ -13,19 +16,28 @@ def plot_robot_geometry(robot_info, color='k'):
         plot(global_robot_frames[:,0,3],
                 global_robot_frames[:,2,3], color ,linewidth=2)
 
-if __name__ == '__main__':
+        tool_pos = nzip(robot_info['tcp'][:3, 3],
+                        robot_info['flange'][:3, 3]).T
+        plot(tool_pos[:,0],
+             tool_pos[:,2], color='g' ,linewidth=2)
 
+if __name__ == '__main__':
         j1 =  0
-        j2 =  10
-        j3 =  10
+        j2 =  0
+        j3 =  0
         j4 =  0
-        j5 =  10
+        j5 =  0
         j6 =  0
 
         joint_values = [j1, j2, j3, j4, j5, j6]
+        tool = hom(0,0,0,[0.1,0,0])
 
+        dh_table['tool'] = tool
         info = forward_kinematics(*joint_values, **dh_table)
-        pose = info['T44']
+        # multiplication from left: rotation in base to base
+        # multiplication from right: rotation in tool to base
+        tcp = info['tcp'].dot(hom(ori(-90,90,0)))
+        pose = hom(tcp[:3,:3],[0.4, 0, 0.3])
 
         s = calc_invkin_irb140(pose, raw_solutions=True)
         ik_up = forward_kinematics(*s[0], **dh_table)
@@ -38,5 +50,6 @@ if __name__ == '__main__':
         plot_robot_geometry(ik_up_back,'b--')
         plot_robot_geometry(ik_down,'r')
         plot_robot_geometry(ik_down_back,'r--')
+        grid()
         axes().set_aspect('equal', 'datalim')
         show()
