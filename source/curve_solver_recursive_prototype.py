a4299171a098febb262464d143cc223e9b6f864b
Jonny Sjöberg <jonny.sjoberg.229@student.lu.se>
2016-05-07 11:54:41 +0200

    moved recursive path-solver code to denavit_hartenberg140.py

--- source/curve_solver_recursive_prototype.py
+++ source/curve_solver_recursive_prototype.py
@@ -11,7 +11,7 @@ from plane_relative import generate_symmetric_curve,\
                            get_transformed_points, apply_transform_on_frames
 
 from denavit_hartenberg140 import forward_kinematics, DH_TABLE as dh_table,\
-     inverse_kinematics_curve,\
+     inverse_kinematics_curve, find_single_path, \
      calc_valid_invkin_irb140 as invkin
 
 from denavit_hartenberg import homogenous_matrix as hom
@@ -36,70 +36,20 @@ import utils
 numpy.set_printoptions(precision=2)
 numpy.set_printoptions(suppress=True)
 
-    
-def __find_solution_path(res, result, curr_point=0, curr_ind=0, tol=20.0):
-    p_curr = res[curr_point] #get the solutions for current point
-    solution = p_curr[curr_ind]
-    if curr_point+1 >= len(res):
-        result.append(solution)
-        return 
-    else:
-        p_dest = res[curr_point+1]
-    sol_diff = map(norm, solution - p_dest)
-    z = zip(p_dest, sol_diff, range(len(p_dest)))
-    z_sorted = sorted(z, key=lambda x: x[1])
-    z_sorted, _, index = zip(*z_sorted)
-    z, _, _ = zip(*z)
-    sel_z = z_sorted[0]
-    sel_ind = index[0]
-    if sol_diff[sel_ind] > tol:
-        del result[:]
-        return
-    else:
-        result.append(solution)
-    print curr_point
-    __find_solution_path(res, result, curr_point = curr_point+1,curr_ind=sel_ind)
-
-def __find_path(ik_curve, index):
-    path = []
-    __find_solution_path(ik_curve, path,
-                         curr_point=0, curr_ind=index)
-    return path
-
-def find_paths(ik_curve):
-    result = ik_curve
-    total = []
-    with utils.timing.Timer() as t:
-        for index in xrange(len(result[0])):
-            path = __find_path(result, index)
-            if path:
-                print 'FOUND ONE' 
-                total.append(list(path))
-        return mat(total)
-
-def find_single_path(ik_curve):
-    result = ik_curve
-    total = []
-    with utils.timing.Timer() as t:
-        for index in xrange(len(result[0])):
-            path = __find_path(result, index)
-            if path:
-                return mat(path)
-
 if __name__ == '__main__':
-    for count in xrange(1):
-        dh_table['tool'] = hom(0,-40,0,[0,0,0.05])
-        T44 = hom(ori(-90,90,0),[0.5,0,0.5])
+    for count in xrange(0,50,11):
+        dh_table['tool'] = hom(0,0,0,[0.0,0,0.1])
+        T44 = hom(-90,180,0,[0.3,0,0.5])
 
         # generate a curve in the last global robot-frame
         num_p = 50
         point_matrix = generate_symmetric_curve(num_points=num_p,
-                                                ampl_factor=0.1)
+                                                ampl_factor=0.3)
         point_matrix_tf = get_transformed_points(T44, point_matrix)
 
         # generate angles
-        rot  = numpy.linspace(0,0)
-        tilt = numpy.linspace(0,0)
+        rot  = numpy.linspace(0,180)
+        tilt = numpy.linspace(-40,40)
         skew = numpy.linspace(0,0)
 
         # generate frames
@@ -119,7 +69,7 @@ if __name__ == '__main__':
         path = find_single_path(result)
         print path
         
-        robot_info = forward_kinematics(*path[37], **dh_table)
+        robot_info = forward_kinematics(*path[count], **dh_table)
 
         pl = StPlot()
         pl.draw_robot(robot_info['robot_geometry_global'])
