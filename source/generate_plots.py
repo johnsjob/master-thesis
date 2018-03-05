e824b2f494da89f8b636be41dcd18d87197262b8
Jonny Sjöberg <jonny.sjoberg.229@student.lu.se>
2018-03-05 22:31:17 +0100

    recreated the parsing script, sync with plot settings and add to generate_plots.py

--- source/generate_plots.py
+++ source/generate_plots.py
@@ -2,9 +2,11 @@ import robot_station_verification2d
 import jacobian_curve_solver
 import calibration_algorithm_newer
 import calibration_algorithm_verification_newer22
+import hidparser_file
 
 if __name__ == '__main__':
     robot_station_verification2d.main()
     jacobian_curve_solver.main()
+    hidparser_file.main()
     calibration_algorithm_newer.main()
     calibration_algorithm_verification_newer22.main()
--- source/hidparser_file.py
+++ source/hidparser_file.py
@@ -4,23 +4,27 @@ import json
 import operator
 from collections import OrderedDict
 import time
+import os.path as path
 
 import hidparser
 from numpy import array as mat
 
+from pylab import plot, show, xticks, yticks, xlabel, ylabel, grid, savefig, clf
+
+from plotsettings import PlotSettings
+
+basepath = r"C:\Users\***REMOVED***\Dropbox\exjobb\results\hid_capture"
 
 def main():
     lines = None
-    with open(r"C:\Users\***REMOVED***\Dropbox\exjobb\results\hid_capture\raw_hid_data.txt") as fp:
+    with open(path.join(basepath, "raw_hid_data.txt")) as fp:
         lines = fp.readlines()
     reports = [hidparser.HIDReport(hex_report = x.split(" ")[3:-1]).data for x in lines]
-
 
     positions = mat([[p['x'], p['y'], p['tiltx']/100.0, p['tilty']/100.0, p['twist']/100.0] for p in reports])
     pos = mat(filter(hidparser.filter_func, positions))
     pos_err = mat(filter(lambda x: not hidparser.filter_func(x), positions))
 
-    from pylab import *
     ad_to_mm = 0.3
     mm_to_cm = 0.1
     correction_factor = 0.0625
@@ -28,25 +32,37 @@ def main():
 
     plot( (pos[:,0] - pos[0,0])*cm,
          -(pos[:,1] - pos[0,1])*cm, 'b')
-    xlabel('x [cm]')
-    ylabel('y [cm]')
+    xlabel('x [cm]', fontsize=PlotSettings.label_size)
+    ylabel('y [cm]', fontsize=PlotSettings.label_size)
+    xticks(fontsize=PlotSettings.tick_size)
+    yticks(fontsize=PlotSettings.tick_size)
     grid()
-    show()
+    savefig(path.join(basepath, "johnnys_local.png"),
+        bbox_inches='tight')
+    clf()
 
     plot(positions[:,0], -positions[:,1], 'r')
-    xlabel('x [ad]')
-    ylabel('y [ad]')
+    xlabel('x [ad]', fontsize=PlotSettings.label_size)
+    ylabel('y [ad]', fontsize=PlotSettings.label_size)
+    xticks(fontsize=PlotSettings.tick_size)
+    yticks(fontsize=PlotSettings.tick_size)
     grid()
-    show()
+    savefig(path.join(basepath, "johnnys_errors_global.png"),
+        bbox_inches='tight')
+    clf()
 
     plot( positions[:,0]*cm,
          -positions[:,1]*cm, 'r', linewidth=0.3)
     plot( pos[:,0]*cm,
          -pos[:,1]*cm, 'b')
-    xlabel('x [cm]')
-    ylabel('y [cm]')
+    xlabel('x [cm]', fontsize=PlotSettings.label_size)
+    ylabel('y [cm]', fontsize=PlotSettings.label_size)
+    xticks(fontsize=PlotSettings.tick_size)
+    yticks(fontsize=PlotSettings.tick_size)
     grid()
-    show()
+    savefig(path.join(basepath, "johnnys_all.png"),
+        bbox_inches='tight')
+    clf()
 
 
 if __name__ == '__main__':
